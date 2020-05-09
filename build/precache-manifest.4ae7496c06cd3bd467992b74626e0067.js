1d1c6dd93e6afb394dcf15ef9c1ec983242d13dc
clicktravel-craig <craig.handley@clicktravel.com>
2020-05-09 16:15:07 +0100

    added heroku config

---
+++ build/precache-manifest.4ae7496c06cd3bd467992b74626e0067.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "c08d44066050d00234acd394c59ea330",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "e50784e9a651826db559",
+    "url": "/static/css/main.75fe3be6.chunk.css"
+  },
+  {
+    "revision": "e50784e9a651826db559",
+    "url": "/static/js/main.e50784e9.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "48b1e95b4909634351f2",
+    "url": "/static/js/vendors~main.48b1e95b.chunk.js"
+  },
+  {
+    "revision": "b585c5a78b86cc35a13f5006b66a8c0a",
+    "url": "/static/media/about_01.b585c5a7.jpg"
+  },
+  {
+    "revision": "6b082edd845faee61310741728a8f998",
+    "url": "/static/media/banner.6b082edd.jpg"
+  },
+  {
+    "revision": "c27bb5c0790a7375ad8c675e741410a6",
+    "url": "/static/media/jlh-languages.c27bb5c0.png"
+  },
+  {
+    "revision": "92305e7223b801b427b8a6a3b1c3ac44",
+    "url": "/static/media/pic01.92305e72.jpg"
+  },
+  {
+    "revision": "7a1cc4cec8edb4bfc754642868db0693",
+    "url": "/static/media/pic02.7a1cc4ce.jpg"
+  },
+  {
+    "revision": "28b540b353c1c2915ec58a845ac3f2d9",
+    "url": "/static/media/pic03.28b540b3.jpg"
+  },
+  {
+    "revision": "9aedd6f1b393a718bb1db19cdf3887aa",
+    "url": "/static/media/pic04.9aedd6f1.jpg"
+  },
+  {
+    "revision": "ee1864421c844f4af8305a3112d8b73f",
+    "url": "/static/media/pic05.ee186442.jpg"
+  },
+  {
+    "revision": "542ba962741138d27b795723c2f6f627",
+    "url": "/static/media/pic06.542ba962.jpg"
+  }
+]);
